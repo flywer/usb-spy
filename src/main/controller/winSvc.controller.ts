817a55114e5ac56af0d25621afda23439c6a88af
wangcb <1781998918@qq.com>
2023-03-09 15:01:02 +0800

    与windows服务间通讯

---
+++ src/main/controller/winSvc.controller.ts
@@ -0,0 +1,13 @@
+import {Controller} from "einf";
+
+/**
+ * @description: 与windows服务间通讯
+ * @author: wangcb
+ * @date: 2023/3/9 11:40
+ **/
+@Controller()
+export class WinSvcController {
+    constructor() {
+    }
+
+}
